e1ca4bc3c1051d91806bcd4ed44cceeb50750d70
cztamas <tamas.czank@gmail.com>
2024-06-15 13:34:53 +0000

    try to hack together a temporary version to non-CLI usage

--- src/impl/PLV8ifyCLI.ts
+++ src/impl/PLV8ifyCLI.ts
@@ -1,22 +1,22 @@
 import fs, { Mode } from 'fs'
-import { BundlerType } from 'src/helpers/ParseCLI.js'
-import { Bundler } from 'src/interfaces/Bundler.js'
+import { BundlerType } from 'src/helpers/ParseCLI'
+import { Bundler } from 'src/interfaces/Bundler'
 import {
   BuildArgs,
   GetPLV8SQLFunctionsArgs,
   PLV8ify,
   Volatility,
-} from 'src/interfaces/PLV8ify.js'
+} from 'src/interfaces/PLV8ify'
 import {
   TSCompiler,
   TSFunction,
   TSFunctionParameter,
-} from 'src/interfaces/TSCompiler.js'
+} from 'src/interfaces/TSCompiler'
 import { match } from 'ts-pattern'
 
-import { BunBuild } from './BunBuild.js'
-import { EsBuild } from './EsBuild.js'
-import { TsMorph } from './TsMorph.js'
+import { BunBuild } from './BunBuild'
+import { EsBuild } from './EsBuild'
+import { TsMorph } from './TsMorph'
 
 interface GetPLV8SQLFunctionArgs {
   fn: TSFunction
